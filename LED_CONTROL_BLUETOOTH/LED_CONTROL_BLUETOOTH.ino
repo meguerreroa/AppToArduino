41ea1cbea2b0a51f69916cb0981b996d8ad021a2
meguerreroa <36995005+meguerreroa@users.noreply.github.com>
2019-02-20 21:30:51 -0500

    Add files via upload
    Added original files

---
+++ LED_CONTROL_BLUETOOTH/LED_CONTROL_BLUETOOTH.ino
@@ -0,0 +1,44 @@
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+
+int estado = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop()
+{
+  if( Serial.available()>0)
+  {
+    estado = Serial.read();
+  }
+  switch( estado)
+  {
+    case 'a':
+    digitalWrite(led1, HIGH);
+    break;
+    case 'b':
+    digitalWrite(led1, LOW);
+    break;
+    
+    case 'c':
+    digitalWrite(led2, HIGH);
+    break;
+    case 'd':
+    digitalWrite(led2, LOW);
+    break;
+	
+	case 'e':
+    digitalWrite(led3, HIGH);
+    break;
+    case 'f':
+    digitalWrite(led3, LOW);
+    break;
+  }
+}
